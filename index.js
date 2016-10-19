ffcca5a6422b7a21509f406ff5c4cacf3ef9db58
Forrest Blanton <fblanton@gmail.com>
2016-10-19 13:39:40 -0700

    Adds mongodb and a get route.

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const express = require('express')
-const app = express()
+const { MongoClient } = require('mongodb')
 
-app.use(express.static('./public'))
-app.listen(7000)
+const URI = 'mongodb://localhost:27017/todo-app'
+const PORT = 7000
+
+MongoClient.connect(URI, (err, db) => {
+  if (err) {
+    // eslint-disable-next-line
+    console.error(err);
+    process.exit(1);
+  }
+
+  const app = express()
+  app.use(express.static('./public'))
+
+  const todos = db.collection('todos')
+  app.get('/todos', (req, res, next) => {
+    todos
+      .find({})
+      .toArray()
+      .then(todos => res.json(todos))
+      .catch(next)
+  })
+
+  app.use((err, req, res, next) => {
+    console.error(err);
+    res.sendStatus(500);
+  })
+  app.listen(PORT)
+})
